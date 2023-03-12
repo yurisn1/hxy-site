3b27354ddfd237d0701aba467d0d156fef5a3da3
Yurisn1 <dev.yurisn@gmail.com>
2023-03-12 20:46:24 -0300

    Updating app routing to get colaborator components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ColaboratorComponent } from './colaborator/colaborator.component';
+
+const routes: Routes = [
+  { path: '', component: ColaboratorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
