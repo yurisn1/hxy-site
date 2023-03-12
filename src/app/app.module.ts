4d32bf4db6a5b36eeaf6aa6e3cf0fefc62cfe307
Yurisn1 <dev.yurisn@gmail.com>
2023-03-12 14:40:31 -0300

    :sparkles: Added main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponentComponent } from './main-component/main-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponentComponent
   ],
   imports: [
     BrowserModule,
