62c495d29aa61ebdf5292de8ffcdac74be74fc12
Yurisn1 <dev.yurisn@gmail.com>
2023-03-15 13:03:49 -0300

    ✨ Added index page component

---
+++ src/app/components/index-page/index-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-index-page',
+  templateUrl: './index-page.component.html',
+  styleUrls: ['./index-page.component.css']
+})
+export class IndexPageComponent {
+ 
+}
