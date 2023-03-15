c9c559fc38d6ef18817a4490e210f757f4472cf4
Yurisn1 <dev.yurisn@gmail.com>
2023-03-15 08:20:37 -0300

    🎨 Improved structure of colaborator component

--- src/app/colaborator/colaborator.component.ts
+++ src/app/colaborator/colaborator.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from '../service/data.service';
 
 @Component({
   selector: 'app-colaborator',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./colaborator.component.css']
 })
 export class ColaboratorComponent {
+  colaborators:any;
+  constructor(private dataService:DataService) { }
+  ngOnInit(): void {
+    this.getColaboratorsData();
+  }
 
+  getColaboratorsData(){
+    this.dataService.getColaborators().subscribe(res => {
+      this.colaborators = res;
+      this.colaborators.forEach(colaborator => {
+        colaborator.stacks = JSON.parse(colaborator.stacks);
+        colaborator.stacks.join(',');
+
+      });
+    });
+  }
 }
