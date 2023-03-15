a0f4cb7711617194291c7dce5f1ad58bdb247626
Yurisn1 <dev.yurisn@gmail.com>
2023-03-15 08:11:54 -0300

    ✨ Added new routes to project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ColaboratorComponent } from './colaborator/colaborator.component';
+import { RegisterColaboratorComponent } from './register-colaborator/register-colaborator.component';
 
 const routes: Routes = [
-  { path: '', component: ColaboratorComponent }
+
+  { path: 'registros', component: ColaboratorComponent },
+  { path: ':name/registrar', component: RegisterColaboratorComponent }
 ];
 
 @NgModule({
